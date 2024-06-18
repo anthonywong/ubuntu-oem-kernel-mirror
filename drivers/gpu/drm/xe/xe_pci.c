393f189630792a12a5e1cb0ec0c00c155d660662
Timo Aaltonen <timo.aaltonen@canonical.com>
2024-06-18 12:11:25 +0300

    UBUNTU: SAUCE: drm/xe: Drop force_probe requirement for Lunar Lake.
    BugLink: http://bugs.launchpad.net/bugs/2076209
    
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>

--- drivers/gpu/drm/xe/xe_pci.c
+++ drivers/gpu/drm/xe/xe_pci.c
@@ -338,7 +338,7 @@ static const struct xe_device_desc mtl_desc = {
 static const struct xe_device_desc lnl_desc = {
 	PLATFORM(LUNARLAKE),
 	.has_display = true,
-	.require_force_probe = true,
+	.require_force_probe = false,
 };
 
 static const struct xe_device_desc bmg_desc = {
