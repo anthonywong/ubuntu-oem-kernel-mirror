708a2ef42f4e1efef156d525ee38d822eef61b31
Mika Westerberg <mika.westerberg@linux.intel.com>
2025-11-19 12:49:30 +0200

    thunderbolt: Set tb->root_switch to NULL when domain is stopped
    BugLink: https://bugs.launchpad.net/bugs/2160699
    
    Similarly what we do with the firmware connection manager. This makes
    tb_xdp_handle_request() return error to the remote host. However, we
    need to make sure we keep the uuid alive so that we can reply until the
    whole domain is released.
    
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    (cherry picked from commit e56249d8a68e712f3b60e1f3fdbb5b4fea146468)
    Signed-off-by: Chia-Lin Kao (AceLan) <acelan.kao@canonical.com>

--- drivers/thunderbolt/tb.c
+++ drivers/thunderbolt/tb.c
@@ -3056,6 +3056,7 @@ static void tb_stop(struct tb *tb)
 		tb_tunnel_put(tunnel);
 	}
 	tb_switch_remove(tb->root_switch);
+	tb->root_switch = NULL;
 	tcm->hotplug_active = false; /* signal tb_handle_hotplug to quit */
 }
 
