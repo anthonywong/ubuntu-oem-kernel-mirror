ce61745e84134ce01578bd1ece95a36883462985
Charles Keepax <ckeepax@opensource.cirrus.com>
2026-05-12 11:30:05 +0100

    soundwire: Add a helper function to wait for device initialisation
    BugLink: https://bugs.launchpad.net/bugs/2164967
    
    Add a new helper function to wait for the device to enumerate
    and be initialised by the SoundWire core. Most of the SoundWire
    drivers have very similar boiler plate code in their runtime
    resume, and that boiler plate tends to access various internals
    of the SoundWire structs which is a mild layering violation.
    
    Adding a new core helper function greatly eases both of these
    issues.
    
    Acked-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Charles Keepax <ckeepax@opensource.cirrus.com>
    Link: https://patch.msgid.link/20260512103022.1154645-2-ckeepax@opensource.cirrus.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 3492e8b494c18028044d4a2e03db5c7331fbd789)
    Signed-off-by: Chris Chiu <chris.chiu@canonical.com>
    Acked-by: Kuan-Ying Lee <kuan-ying.lee@canonical.com>
    Signed-off-by: Kuan-Ying Lee <kuan-ying.lee@canonical.com>

--- include/linux/soundwire/sdw.h
+++ include/linux/soundwire/sdw.h
@@ -1088,6 +1088,8 @@ int sdw_stream_remove_slave(struct sdw_slave *slave,
 
 int sdw_slave_get_scale_index(struct sdw_slave *slave, u8 *base);
 
+int sdw_slave_wait_for_init(struct sdw_slave *slave, int timeout_ms);
+
 /* messaging and data APIs */
 int sdw_read(struct sdw_slave *slave, u32 addr);
 int sdw_write(struct sdw_slave *slave, u32 addr, u8 value);
@@ -1114,6 +1116,24 @@ static inline int sdw_stream_add_slave(struct sdw_slave *slave,
 
 static inline int sdw_stream_remove_slave(struct sdw_slave *slave,
 					  struct sdw_stream_runtime *stream)
+{
+	WARN_ONCE(1, "SoundWire API is disabled");
+	return -EINVAL;
+}
+
+static inline struct device *of_sdw_find_device_by_node(struct device_node *np)
+{
+	WARN_ONCE(1, "SoundWire API is disabled");
+	return NULL;
+}
+
+static inline int sdw_slave_get_current_bank(struct sdw_slave *sdev)
+{
+	WARN_ONCE(1, "SoundWire API is disabled");
+	return -EINVAL;
+}
+
+static inline int sdw_slave_wait_for_init(struct sdw_slave *slave, int timeout_ms)
 {
 	WARN_ONCE(1, "SoundWire API is disabled");
 	return -EINVAL;
